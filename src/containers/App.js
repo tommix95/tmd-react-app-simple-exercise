6261cfa587a5988f1bb805a00f6236d2d1d8018b
tommix95 <tommixweb@gmail.com>
2020-04-18 19:51:50 +0200

    First commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/searchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from  '../components/ErrorBoundry';
 //import {robots} from './robots';
 import './App.css';
 
@@ -36,7 +37,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                    <CardList robots={filterRobots} />
+                    <ErrorBoundry>
+                        <CardList robots={filterRobots} />
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         );
