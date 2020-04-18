79a3f48d9dcdfdcd9c1a215a8197528931e37d80
tommix95 <tommixweb@gmail.com>
2020-04-18 19:53:33 +0200

    Updates

---
+++ precache-manifest.2ae5c76076fe8a29bf377fb667a27644.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cc60ead4c98725a77d28b319893d51",
+    "url": "/tmd-react-app-simple-exercise/index.html"
+  },
+  {
+    "revision": "dc8318b9de318c267c9c",
+    "url": "/tmd-react-app-simple-exercise/static/css/2.2dc38268.chunk.css"
+  },
+  {
+    "revision": "e436596d3ffc55999acd",
+    "url": "/tmd-react-app-simple-exercise/static/css/main.fa41de12.chunk.css"
+  },
+  {
+    "revision": "dc8318b9de318c267c9c",
+    "url": "/tmd-react-app-simple-exercise/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tmd-react-app-simple-exercise/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e436596d3ffc55999acd",
+    "url": "/tmd-react-app-simple-exercise/static/js/main.695d0c72.chunk.js"
+  },
+  {
+    "revision": "16748258107f01f2ca80",
+    "url": "/tmd-react-app-simple-exercise/static/js/runtime-main.d7537916.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/tmd-react-app-simple-exercise/static/media/SEGA.12a3db8b.woff"
+  }
+]);
